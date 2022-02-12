19d20d4bff97ab0dd4f33d2dfae11709fd47f4c7
Bag Boy <73358379+BAGBOY555@users.noreply.github.com>
2022-02-12 17:06:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -35,7 +35,7 @@ URL | filename | username | password"""
 Telegram ID: <code>{}</code>
 Plan name: Free Cloned User
 Expires on: 31/12/2020
-mention : <a href="link">tg://settings/</a>"""
+mention : <a href=tg://settings/>hhh/</a>"""
     HELP_USER = """Hai am URL Uploader bot..
     
 1. Send url (Link|New Name with Extension).
