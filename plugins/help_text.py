d8573f1c5aae7bba31f4793d6d3f42d81a8312d3
Bag Boy <73358379+BAGBOY555@users.noreply.github.com>
2022-02-12 16:21:32 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -60,7 +60,7 @@ async def start(bot, update):
     # logger.info(update)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(message.from_user.mention),
+        text=Translation.START_TEXT.format(update.from_user.mention),
         reply_to_message_id=update.message_id
     )
 
